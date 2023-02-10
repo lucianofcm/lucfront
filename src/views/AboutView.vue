a8dc8ccf8e253fb1bb7c0b12cbe356ed535263e9
lucianofcm <lucweb@gmail.com>
2023-02-09 22:59:37 -0300

    make it better

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -13,27 +13,3 @@
   }
 }
 </style>
-const app = new Vue({
-  data: () => ({images: null}),
-  template: `
-    <div>
-      <input type="file" @change="uploadFile" ref="file">
-      <button @click="submitFile">Upload!</button>
-    </div>
-  `,
-  methods: {
-    uploadFile() {
-      this.Images = this.$refs.file.files[0];
-    },
-    submitFile() {
-      const formData = new FormData();
-      formData.append('file', this.Images);
-      const headers = { 'Content-Type': 'multipart/form-data' };
-      axios.post('https://httpbin.org/post', formData, { headers }).then((res) => {
-        res.data.files; // binary representation of the file
-        res.status; // HTTP status
-      });
-    }
-  }
-});
-app.$mount("#content");
