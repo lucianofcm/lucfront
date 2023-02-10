4f2ba6d43169fbfd2f6c956ec66d80209e3dcc72
lucianofcm <lucweb@gmail.com>
2023-02-10 02:16:50 -0300

    .2

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <div class="large-12 medium-12 small-12 cell">
       <label>File
-        <input type="file" id="file" ref="file" v-on:change="handleFileUpload"/>
+        <input type="file" id="file" ref="file" v-on:change="(e)=>handleFileUpload(e)"/>
       </label>
       <button v-on:click="submitFile()">Submit</button>
     </div>
